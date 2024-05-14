f1e5fccfc2f90ad857821f5a7e79ecc7ccd0d761
sophie <>
2024-05-14 17:13:19 +0200

    workflow

---
+++ Juniper-Apstra/surveys/associate-survey-to-workflow-template.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "USAGE: associate-survey-to-template <template id> <survey json file>"
+  exit
+fi
+
+TEMPLATE_ID=$1
+SURVEY_FILE=$2
+
+curl -u sophie:TSL.Dem0s! -k -X POST --header 'Content-Type: application/json' -d @./$SURVEY_FILE https://dev-controller-aap.apps.ocp4aapodf.p123.cesc-test.nca.ihost.com/api/v2/workflow_job_templates/$TEMPLATE_ID/survey_spec/
